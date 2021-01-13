9030c5e48d44c330ec8baa2f9876cd2052729cb8
macjohn96 <john6996@hotmail.es>
2021-01-13 19:53:32 +0100

    código sin javadoc DNI 23323884K

---
+++ src/analistaed_23323884k/AnalistaED_23323884K.java
@@ -0,0 +1,33 @@
+package analistaed_23323884k;
+import java.util.*;
+
+public class AnalistaED_23323884K {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public AnalistaED_23323884K(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
+        AnalistaED_23323884K emp = new AnalistaED_23323884K("JOHN CARRANZA ROSERO", "23323884K");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto 
+		+ " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+
+}
